387b8b36af1339ab7095a7d8142e2c760e185df1
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2021-12-05 12:30:48 +0200

    fixed JS collector

--- application/SampleOpenTelemetry/assets/document-load.js
+++ application/SampleOpenTelemetry/assets/document-load.js
@@ -1,27 +1,43 @@
-﻿import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { CollectorTraceExporter } from '@opentelemetry/exporter-collector-grpc';
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
-import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
-import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
-import { ZoneContextManager } from '@opentelemetry/context-zone';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
+﻿import * as opentelemetry from "@opentelemetry/api";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { Resource } from "@opentelemetry/resources";
+import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
+import { ConsoleSpanExporter, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { DocumentLoadInstrumentation } from "@opentelemetry/instrumentation-document-load";
+import { XMLHttpRequestInstrumentation } from "@opentelemetry/instrumentation-xml-http-request";
+import { UserInteractionInstrumentation } from "@opentelemetry/instrumentation-user-interaction";
+import { ZoneContextManager } from "@opentelemetry/context-zone";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
 
-const provider = new WebTracerProvider();
+opentelemetry.diag.setLogger(
+    new opentelemetry.DiagConsoleLogger(),
+    opentelemetry.DiagLogLevel.VERBOSE
+);
 
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-provider.addSpanProcessor(new SimpleSpanProcessor(new CollectorTraceExporter()));
-
-provider.register({
-    // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
-    contextManager: new ZoneContextManager(),
+const provider = new WebTracerProvider({
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: "basic-service"
+    })
 });
 
-// Registering instrumentations
+const config = {
+    url: "http://localhost:4318/v1/traces",
+    headers: {},
+    concurrencyLimit: 10
+};
+
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter(config)));
+
+provider.register({
+    contextManager: new ZoneContextManager()
+});
+
 registerInstrumentations({
     instrumentations: [
         new DocumentLoadInstrumentation(),
         new UserInteractionInstrumentation(),
         new XMLHttpRequestInstrumentation()
-    ],
+    ]
 });
